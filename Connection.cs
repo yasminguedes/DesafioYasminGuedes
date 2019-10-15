7a21950294ed74e9f99d9b9209d8df7655c3e275
yasminguedes <yasmin_antonia@hotmail.com>
2019-10-15 14:34:07 -0300

    segunda versão do projeto

---
+++ Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace ProjetoDesafio
+{
+    public class Connection
+    {
+        private static readonly Connection instanciaFireBird = new Connection();
+        private Connection(){}
+
+        public static Connection getInstancia()
+        {
+            return instanciaFireBird;
+        }
+
+        public FbConnection getConexao()
+        {
+            string conn = ConfigurationManager.ConnectionStrings["FireBirdConnectionString"].ToString();
+            return new FbConnection(conn);
+        }
+
+        
+    }
+}
---
+++ Model/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDesafio
+{
+    public class Marca
+    {
+        public int idMarca { get; set; }
+        public string nomeMarca { get; set; }
+    }
+}
